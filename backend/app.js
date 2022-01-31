a6ab49c7ecab194e6e2e16f1707f58cfbd445f19
Havvaozlk <ozcelik.havva@icloud.com>
2022-01-31 16:45:58 +0100

    installation mongoose, ajout de l'URI en utilisant dotenv

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,36 @@
+//importation d'express
 const express = require('express');
-
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/user');
 const app = express();
 
-app.use((req, res) => {
-   res.json({ message: 'Votre requête a bien été reçue !' }); 
-});
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+    console.log('Requête reçue !');
+    next();
+  });
+  
+  app.use((req, res, next) => {
+    res.status(201);
+    next();
+  });
+  
+  app.use((req, res, next) => {
+    res.json({ message: 'Votre requête a bien été reçue !' });
+    next();
+  });
+  
+  app.use((req, res, next) => {
+    console.log('Réponse envoyée avec succès !');
+  });
+  
+  app.use('/api/auth', userRoutes);
 
 module.exports = app;
