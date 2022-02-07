dbf0f488dc6c7c6aff60f60b5b17b1dba192b3fd
Havvaozlk <ozcelik.havva@icloud.com>
2022-02-07 17:47:34 +0100

    suppression des app.use non nécessaire

--- backend/app.js
+++ backend/app.js
@@ -2,52 +2,55 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
-const saucesRoutes = require('./routes/sauces');
-const userRoutes = require('./routes/user');
 const path = require('path');
-const app = express();
 
 require('dotenv').config();
 
+const saucesRoutes = require('./routes/sauce');
+const userRoutes = require('./routes/user');
+
 mongoose.connect(process.env.MONGO_URI,
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-  app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
+  const app = express();
 
 app.use((req, res, next) => {
-    console.log('Requête reçue !');
-    next();
-  });
-  
-  app.use((req, res, next) => {
-    res.status(201);
-    next();
-  });
-  
-  app.use((req, res, next) => {
-    res.json({ message: 'Votre requête a bien été reçue !' });
-    next();
-  });
-  
-  app.use((req, res, next) => {
-    console.log('Réponse envoyée avec succès !');
-  });
-  
-  app.use(bodyParser.json());
-app.use(express.json());
-app.use(express.urlencoded({extended: false }));
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
-  app.use('/images', express.static(path.join(__dirname, 'images')));
-  app.use('/api/sauces', saucesRoutes);
-  app.use('/api/auth', userRoutes);
+// app.use((req, res, next) => {
+//   console.log('Requête reçue !');
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   res.status(201);
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   res.json({ message: 'Votre requête a bien été reçue !' });
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   console.log('Réponse envoyée avec succès !');
+// });
+
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
--- backend/models/sauce.js
+++ backend/models/sauce.js
@@ -5,13 +5,13 @@ const sauceSchema = mongoose.Schema({
   name: { type: String, required: true },
   manufacturer: { type: String, required: true },
   description: { type: String, required: true },
-  mainPepper: { type: Number, required: true },
-    imageUrl: { type: String, required: true},
-    heat: { type: Number, required: true },
-    likes: { type: Number },
-    dislikes: { type: Number },
-    usersLiked: { type: [String] },
-    usersDisliked: { type: [String] },
+  mainPepper: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  heat: { type: Number, required: true },
+  likes: { type: Number, required: true },
+  dislikes: { type: Number, required: true },
+  usersLiked: { type: [String], required: true },
+  usersDisliked: { type: [String], required: true },
 });
 
 module.exports = mongoose.model('Sauce', sauceSchema); 
