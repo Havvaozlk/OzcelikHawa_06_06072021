02b24e828751bf3ea66ad1874db32d40f65f9ab2
Havvaozlk <ozcelik.havva@icloud.com>
2022-02-08 08:33:53 +0100

    ajout helmet

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 //importation d'express
 const express = require('express');
+const helmet = require("helmet");
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
@@ -46,6 +47,7 @@ app.use((req, res, next) => {
 // });
 
 app.use(bodyParser.json());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
